a3e36c450d6c98edf3993e33204ba4aef8f65dd1
Alex <izofatov2007@gmail.com>
2018-01-11 08:44:47 +0200

    chenge img & updated

--- js/main.js
+++ js/main.js
@@ -1,9 +1,38 @@
+// Loader
 document.addEventListener( 'DOMContentLoaded', function () {
 	setTimeout(function(){
 		let b = document.getElementsByTagName("body");
 		b[0].className += "loaded";
 	}, 3000);
 
+			// $('.ask-fade').fadeOut();
+
+	//Ask question show on scrol
+	$(document).scroll(function(){
+		var y = $(this).scrollTop();
+		var w = window.innerHeight;
+		console.log(w)
+		if (y > ( w/2 )) {
+			$('.wrappContainer').fadeIn();
+		}else{
+			$('.wrappContainer').fadeOut();
+		}
+	})
+	// Ask question down on click
+	var mouseOverActiveElement = false;
+	$('.wrappContainer').on('mouseenter', function(){
+		mouseOverActiveElement = true;
+	}).on('mouseleave', function() {
+		mouseOverActiveElement = false;
+	});
+	$("body").click(function(e){
+		if(!mouseOverActiveElement) {
+			if($(".ask-wrapper input[name=toggle]").is(':checked')){
+				$(".ask-wrapper input[name=toggle]").prop('checked', false);
+			}
+		}
+	})
+	// Ask question blur
 	$(".userInput").focus(function(){
 		$(this).parent().addClass("focus");
 	}).blur(function(){
