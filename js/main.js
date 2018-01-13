3f4e9100f5794efd77aa46bfa6733a4d47b945a5
Alex <izofatov2007@gmail.com>
2018-01-13 05:47:16 +0200

    add velociti.js

--- js/main.js
+++ js/main.js
@@ -4,20 +4,18 @@ document.addEventListener( 'DOMContentLoaded', function () {
 		let b = document.getElementsByTagName("body");
 		b[0].className += "loaded";
 		$('body').css('overflow', 'auto')
+		tlm();
 	}, 3000);
-
-			// $('.ask-fade').fadeOut();
 
 	//Ask question show on scrol
 	$(document).scroll(function(){
-		var y = $(this).scrollTop();
-		var w = window.innerHeight;
-		console.log(w)
-		if (y > ( w/2 )) {
-			$('.wrappContainer').fadeIn();
-		}else{
-			$('.wrappContainer').fadeOut();
-		}
+		// var y = $(this).scrollTop();
+		// var w = window.innerHeight;
+		// if (y > ( w )) {
+		// 	$('.wrappContainer').fadeIn(2000);
+		// }else{
+		// 	$('.wrappContainer').fadeOut(1000);
+		// }
 	})
 	// Ask question down on click
 	var mouseOverActiveElement = false;
@@ -41,5 +39,45 @@ document.addEventListener( 'DOMContentLoaded', function () {
 			$(this).parent().removeClass("focus");
 		}
 	})
+
+	// Login pop
+	// $("#login").on('click', function(){
+	// 	alert('text');
+	// })
+
+	// GSAP
+	function tlm() {
+		let t1 = new TimelineMax();
+		t1
+			.fromTo('.logo' , 1, {y:-100, opacity: 0}, {y: 0, opacity: 1})
+			.fromTo('#login', 1, {y: -100, opacity: 0}, {y: 0, opacity: 1}, "-=0.5", false)
+			.fromTo('.hero h1', 1, {y: -100, opacity: 0}, {y: 0, opacity: 1},"-=0.5")
+			.staggerFromTo('nav a', 3, {opacity:0}, {opacity: 1}, 0.1, "-=0.5")
+			
+	}
+
+	// Velocityjs 
+	// scroll to top
+	const body = document.querySelector('body');
+	const button = document.querySelector('.topBtn');
+	const options = {
+		duration: 1000
+	}	
+	button.addEventListener('click', function(){
+		Velocity(body, 'scroll', options);
+	})
+	// scroll to #
+	const sections = document.querySelectorAll('section');
+	const navigation = document.querySelector('nav');
+	navigation.addEventListener('click', function(e){
+		let target = e.target;
+		if(target && target.tagName.toLowerCase() === 'a') {
+			let attr = target.getAttribute('href').slice(1);
+			console.log(attr);
+			Velocity( sections[attr], 'scroll', options );
+		}
+
+	})
+
 
 });
